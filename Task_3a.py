e03896ffaf68c46f5736702aed1a012e65a43e14
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-04-14 20:01:55 +1000

    Update Task_3a.py

--- Task_3a.py
+++ Task_3a.py
@@ -8,12 +8,14 @@ Created on Wed Apr 12 10:19:06 2023
 
 #Task 3: Interpretation of loop codes
 
+print("Welcome to Task 3(a) solution.\n")        #Calls print function - Welcome message
+
 b = 10                  #Assign the variable: Integer '10' into variable 'b'
 
 while b < 10:           #Initilize while loop. Continues loop for as long as variable "b" is less than integer '10'
     print("Hello")      #Body of while loop. Calls print function for string 'Hello'
     b += 1              #Increases variable 'b' by integer '1'
     
-#This While loop will not inititalize as the condition 'b < 10' has not been satisfied.
+#This while loop will not inititalize as the condition 'b < 10' has not been satisfied.
 
 
