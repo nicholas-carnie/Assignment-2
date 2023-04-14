63637cd2e11fcf31337601c778a5c4b609e4d0fe
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-04-14 19:43:08 +1000

    Update Task_2a.py

--- Task_2a.py
+++ Task_2a.py
@@ -13,10 +13,18 @@ Created on Wed Apr  5 10:15:07 2023
        
 # Run the program. Include a screenshot of the output on the screen in your zip file.
 
-number = 1                              #Assign the variable: Integer '1' into variable 'number'
 
-while number > 0 and number < 10:       #Initialize while loop. Continues loop for as long as variable 'number' is > 0 and 'number' < 10
-    multiplication = number * number    #Body of for loop. multiplication = int(number) * int(number). Converts 'multiplication' to integer.
-    print(number, multiplication)       #Calls print function for variables 'number' & 'multiplication'
-    number = number + 1                 #Sets new value for variable "number" as "number + integer 1"
- 
+print("Welcome to Task 2(a) solution.\n")                                   #Calls print function - Welcome message
+
+print("\nConvert the following for loop into the equivalent while loop")    #Calls print function - Purpose of program
+    
+print("\nFor loop")                             #Calls print function - For loop section
+for i in range(1, 10):                          #For loop example
+    print("{} x {} = {}".format(i,i, i*i))      #For loop example
+
+
+print("\nWhile loop equivalent")                #Calls print function - While loop equivalent
+i = 1                                           #Sets interger 1 into variable i
+while i < 10:                                   #Initiates while loop with condition i less than 10
+    print("{} x {} = {}".format(i, i, i*i))     #Calls print function - Equation & result
+    i+=1                                        #Sets new value for variable "i" as "i + integer i"
