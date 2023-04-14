21ea56c08c52fdff9eb2b91b950a93499b22cae1
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-04-14 19:42:28 +1000

    Update Task_1-2.py

--- Task_1-2.py
+++ Task_1-2.py
@@ -12,7 +12,8 @@ Created on Wed Apr  5 15:19:46 2023
 #Print on the screen, prompting for an input number from the user (e.g., ‘Enter an integer value’)
 #Print the multiplication tables of a given number for 10 times
     
-#An example of the program output when a user enters 5: 5x1=5
+#An example of the program output when a user enters 5: 
+#5 x 1 = 5
 #5 x 2 = 10
 #5 x 3 = 15
 #5 x 4 = 20 
@@ -23,14 +24,17 @@ Created on Wed Apr  5 15:19:46 2023
 #5 x 9 = 45 
 #5 x 10 = 50
 
-multiple = 2                                                #Assign the variable: Integer '2' into variable 'multiple'
+print("Welcome to the Multiplication Table.\n")             #Print function - Welcome message
+print("To get the multiplication of a number.\n")           #Print function - Purpose of program
 
-integer = input("Enter an integer value:\n")                #Calls input function from user. Prints 'Enter an integer value' with an escape character 
+multiple = 1                                                #Assign the variable: Integer '2' into variable 'multiple'
+
+integer = input("Enter a valid number:\n")                  #Calls input function from user. Prints 'Enter an integer value' with an escape character 
 integer = int(integer)                                      #Converts string 'integer' to integer 'integer' into variable 'integer'
 
-for x in range(9):                                          #Intilize for loop with placeholder 'x' in range function. Range executes 9 times only.
+for x in range(10):                                         #Intilize for loop with placeholder 'x' in range function. Range executes 9 times only.
     equation = integer * multiple                           #Body of for loop. Equation = int(integer) * int(multiple). Converts equation to integer.
     print("%s x %s = %s" %(integer, multiple, equation))    #Calls print function. Prints 'equation' formula with use of a tuple.
-    multiple = multiple + 1                                 #Sets new value for variable 'multiple' as 'multiple + integer 1'
+    multiple += 1                                           #Sets new value for variable 'multiple' as 'multiple + integer 1'
 
     
