8f5f05ed36d02437077edd5148046b691db56f67
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-04-14 19:39:39 +1000

    Update Task_1-1a.py

--- Task_1-1a.py
+++ Task_1-1a.py
@@ -8,9 +8,13 @@ Created on Wed Apr  5 13:44:33 2023
 
 # • Print first 10 numbers using while loop (e.g., 1, 2, 3, ...., 8, 9, 10)
 
+print("Welcome to Task 1(a) solution.\n")               #Calls print function with welcome message
+
+print("\nPrint first 10 numbers using while loop")      #Calls print function with purpose of program
+
 x = 1                       #Assign the variable: Integer '1' into variable 'x'
 
-while x > 0 and x < 11:     #Initialize while loop. Continues loop for as long as variable 'x' is > 0 and 'x' < 11
+while x <= 10:              #Initialize while loop. Continues loop for as long as variable 'x' is > 0 and 'x' < 11
     print(x)                #Body of while loop. Calls print function for variable 'x'
-    x = x + 1               #Sets new value for variable 'x' as 'x + integer 1'
+    x += 1                  #Sets new value for variable 'x' as 'x + integer 1'
     
