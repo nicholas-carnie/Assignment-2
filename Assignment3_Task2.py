fb4cb03718505fe3fd4ad850c77748ff4474af65
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-05-11 15:51:24 +1000

    Update Assignment3_Task2.py

--- Assignment3_Task2.py
+++ Assignment3_Task2.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 11 15:43:30 2023
-
-@author: carnie
-"""
-
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 import time
 
 UserDatabase = {}
