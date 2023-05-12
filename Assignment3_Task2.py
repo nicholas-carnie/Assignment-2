69894fbe3caf3bf7b3fb94ee45541442a139bc4a
nicholas-carnie <129142185+nicholas-carnie@users.noreply.github.com>
2023-05-12 14:45:43 +1000

    Update Assignment3_Task2.py

--- Assignment3_Task2.py
+++ Assignment3_Task2.py
@@ -1,38 +1,40 @@
-import time
+import time                                         #Imports time module
 
-UserDatabase = {}
+UserDatabase = {}                                   #Creates dictionary for storing players name & score
 
 def MainMenu():
-    checkInput = None
+    """Function which gives main menu"""            #Doc string which describes purpose of function
+    checkInput = None                               #Resets selection each time function is called
     print("\n" + "===" * 20)
     print("\n**Welcome to Champions Soccer Club**")
     print("Please choose an option from the followings")
     print("1) Add player name and score\n2) Display all the player information and scores\n3) Quit\n")
     print("===" * 20)
-    UserChoice = input("Enter you choice \t:")
-    try:
+    UserChoice = input("Enter you choice \t:")      #User enters either 1, 2 or 3 
+    try:                                            #Try/except statement to reduce program crash in case of incorrect selection
         checkInput = int(UserChoice)
-        if(checkInput > 3 or checkInput <= 0):
+        if(checkInput > 3 or checkInput <= 0):      #Checks if user selection is outside range. Returns to start of function.
             print("Invalid input.\nProvided Options are 1,2 and 3.\nTry Again.")
             MainMenu()
     except ValueError:
         print("Invalid input.\nEnter a valid Input.")
         MainMenu()
 
-    match checkInput:
+    match checkInput:                               #Match/case statement to control flow of program
         case 1:
             AddPlayerDetails()
         case 2:
             ViewUserDetails()
-        case 3:
+        case 3:                                     #Exit selection to end program
             print("**GoodBye.. See you again!*")
             exit()
         
 
 def AddPlayerDetails():
+    """Function to add players name & score (Case #1)"""        #Doc string which describes purpose of function
     Name = input("\nEnter the player name \t:")
     Score = input("Enter the score \t:")
-    try:
+    try:                                                        #Try/except statement to reduce program crash in case of incorrect selection
         checkScore = int(Score)
         if(checkScore > 100 or checkScore < 0):
             print("\n**Incorrect score** The scores should be from 0-100.\nPlease try again!")
@@ -41,30 +43,32 @@ def AddPlayerDetails():
         print("Invalid Entry.\nTry Again")
         MainMenu()
 
-    UserDatabase[Name] = Score
-    UserSelection()
+    UserDatabase[Name] = Score                                  #Assigns Name & Score as key value pairs to dictionary
+    UserSelection()                                             #Calls UserSelection function after AddPlayerDetails function ends
 
 
 def UserSelection():
+    """Function which directs user to either MainMenu or AddPlayerDetails functions"""      #Doc string which describes purpose of function
     userInput = str(input("Do you want to add another player? Enter Y for Yes, N for  No\t:  "))
-    if(userInput.upper() == "Y"):
-        AddPlayerDetails()
-    elif(userInput.upper() == "N"):
-        MainMenu()
-    elif(userInput.upper() != "Y" or userInput.upper() != "N"):
+    if(userInput.upper() == "Y"):                               #Checks user input. Converts to upper case
+        AddPlayerDetails()                                      #Function called for 'Y' selection
+    elif(userInput.upper() == "N"):                             #Checks user input. Converts to upper case
+        MainMenu()                                              #Function called for 'N' selection
+    elif(userInput.upper() != "Y" or userInput.upper() != "N"):     #Condition for incorrect user input
         print("Invalid selection.\nTry Agian")
-        UserSelection()
-
-
+        UserSelection()                                         #Function called for incorrect selection
+                           
+            
 def ViewUserDetails():
+    """Function to view all entries for Userselection function (Case #2)"""                 #Doc string which describes purpose of function
     print("\nPlayer & Score Details")
     print("\n" + '-' * 30 + "\n")
     print("Player" + "\t"*2 + "Score")
-    for item in UserDatabase:
+    for item in UserDatabase:                                   #for loop to display all values for UserDatabase
         print(item  + "\t"*2 + UserDatabase[item])
-    time.sleep(2.0)    #This gives the user a bit of time to view records displayed.
-    MainMenu()
+    time.sleep(2.0)                                             #This gives the user a bit of time to view records displayed.
+    MainMenu()                                                  #Calls MainMenu function after ViewUserDetails function ends
 
 
-# Calling the Entry point of the applicatiion.
-MainMenu()
+                                   
+MainMenu()                                                      # Calling the Entry point of the applicatiion.
